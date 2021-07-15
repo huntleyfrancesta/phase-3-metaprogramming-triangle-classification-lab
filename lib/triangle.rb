3f89b6e0433a8de47f2216d66bc2dd5ef0aaa4b9
huntleyfrancesta <huntleyfrancesta@gmail.com>
2021-07-15 18:19:55 -0400

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+  def initialize(a, b, c)
+    @sides = [a, b, c].sort
+    valid?
+  end
+
+  def valid?
+    if @sides.any?{|side| side <= 0} || @sides[2] >= @sides[0] + @sides[1]
+      raise TriangleError
+    end
+  end
+
+  def kind
+    if @sides.uniq.length == 3
+      :scalene
+    elsif @sides.uniq.length == 2
+      :isosceles
+    else
+      :equilateral
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
